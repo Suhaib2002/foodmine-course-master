f0e7c424f478e3018997e84c5c531f3af1d6ef86
Suhaib2002 <suhaibiqbal7@gmail.com>
2023-08-03 23:21:29 +0530

    sample

---
+++ backend/src/server.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import path from 'path';
+import express from "express";
+import cors from "cors";
+import foodRouter from './routers/food.router';
+import userRouter from './routers/user.router';
+import orderRouter from './routers/order.router';
+import mongoose, {connect, ConnectOptions} from 'mongoose';
+mongoose.set('strictQuery', false);
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:4200"]
+}));
+
+const dbConnect = () => {
+    mongoose.connect(process.env.MONGO_URI!, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    } as ConnectOptions).then(
+        () => console.log("connect successfully"),
+        (error) => console.log(error)
+    )
+}
+dbConnect();
+
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
+})
